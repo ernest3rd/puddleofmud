36e42e5569794936e32fc58c5dd4f4fff95f28d8
Kevan <kevan.m.wiredelta@gmail.com>
2018-05-04 10:16:58 +0300

    fox and copy

---
+++ Assets/foxControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foxControl : MonoBehaviour {
+
+
+	private int moveAmount;
+
+	// Use this for initialization
+	void Start () {
+		moveAmount = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (DustParticleController.counter >= 1){
+			foreach (Transform child in transform) {
+            child.position += Vector3.forward * 0.1F;
+						moveAmount++;
+						child.position += Vector3.zero;
+						if (moveAmount >= 10){
+							child.position += Vector3.forward * 0.0F;
+						}
+
+        }
+		}
+
+	}
+}
